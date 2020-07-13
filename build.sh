953c9f567a171f72c0b02da2ae85a1504b83a756
codeCleanDev <codeclean.dev@gmail.com>
2020-07-13 23:20:38 +0200

    Build info

--- build.sh
+++ build.sh
@@ -20,5 +20,9 @@ USE_SPLITBUILD=1 ./gradlew assembleDebug --no-daemon
 
 SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
-telegram -M "LTerminal: Build completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+if [ -t "dir of output and apk here"]; then
+telegram -M "Build Done"
+else
+telegram -M "Build Failed"
+fi
 telegram -f app/build/outputs/apk/debug/app-debug.apk "LTerminal - Prebuilt APK"
