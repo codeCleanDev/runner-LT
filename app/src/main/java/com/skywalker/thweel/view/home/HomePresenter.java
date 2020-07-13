5f2e38c836dc67d96075da43de3d2483fd0bb4fb
codeCleanDev <codeclean.dev@gmail.com>
2020-07-14 00:47:34 +0200

    files

--- app/src/main/java/com/skywalker/thweel/view/home/HomePresenter.java
+++
@@ -1,7 +0,0 @@
-package com.skywalker.thweel.view.home;
-
-public class HomePresenter {
-    
-    public static String TAG = HomePresenter.class.getSimpleName();
-    
-}
--- initial.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Sync
-sudo install telegram /usr/bin
-telegram -M "Android Github Builder"
-telegram -M "LTerminal: CI Initialized"
