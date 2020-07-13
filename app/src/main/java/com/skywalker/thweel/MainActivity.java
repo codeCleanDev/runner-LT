6b5e43d8b569b25b5d95495875500112506a88bb
codeCleanDev <codeclean.dev@gmail.com>
2020-07-14 00:37:14 +0200

    presenter

--- app/src/main/java/com/skywalker/thweel/MainActivity.java
+++ app/src/main/java/com/skywalker/thweel/MainActivity.java
@@ -88,7 +88,7 @@ public class MainActivity extends  TiActivity<MainActivityPresenter ,MainView> i
     @NonNull
     @Override
     public MainActivityPresenter providePresenter() {
-        return new MainActivityPresenter<>();
+        return new MainActivityPresenter();
     }
 
     @Override
